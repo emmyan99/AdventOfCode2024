cb6b56b02ac3943e644c95976e0debc44a9258ff
emmyan99 <emmy.andreasson@gmail.com>
2024-12-02 21:41:22 +0100

    day2 yippiii

--- day2/day2_b.py
+++ day2/day2_b.py
@@ -1,40 +1,36 @@
 def main():
     reports = list(word.split() for word in open('input.txt'))
 
-    safeCount = 0
-    # safeCount = sum(1 for report in reports if safetyCheck(report))
-    failed_checks = []
+    failed_reports = []
     for report in reports:  
-        safety_check(report, failed_checks)
-    # print(failed_checks)
-    safe_count = len(reports) - len(failed_checks)
-    # print(safeCount)
-    print(failed_checks)
+        safety_check(report, failed_reports)
+    safe_count = len(reports) - len(failed_reports)
 
-    #count = make_safe(failed_checks)
-    #safe_count += count
+    made_safe = 0
+    for report in failed_reports:
+        if make_safe(report):
+            made_safe += 1
+    safe_count += made_safe
 
-    
+    print(safe_count)
 
-def safety_check(report, failed_checks):
+def safety_check(report, failed_reports):
     differences = []
     for i in range(len(report)-1):
         differences.append(int(report[i]) - int(report[i+1]))
-    
-    #print(differences)
+
     res = (all(abs(difference) <= 3 for difference in differences)) and (all(difference > 0 for difference in differences) or all(difference < 0 for difference in differences))
     if not(res):
-        failed_checks.append((differences, report))
+        failed_reports.append(report)
+    return res
 
+def make_safe(report): 
+    placeholder = []
+    for index in range(len(report)):
+        one_val_removed = report[:index] + report[index+1:]
+        if safety_check(one_val_removed, placeholder):
+            return True
+    return False
 
-def make_safe(failed_checks):
-    for check in failed_checks:
-        pass  
-
-
-# if only one number is >3
-# if only one number is negative while all other pos
-# if only one number pos while all other negative 
-    
 if __name__ == '__main__':
     main()
