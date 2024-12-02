311f18f9b4f97880dccc296f1768264466bad011
emmyan99 <emmy.andreasson@gmail.com>
2024-12-02 13:44:21 +0100

    day2b wip

--- day2/day2_b.py
+++ day2/day2_b.py
@@ -1,20 +1,30 @@
-#TODO:
-    
-
-
 def main():
     reports = list(word.split() for word in open('input.txt'))
 
     safeCount = 0
     #safeCount = sum(1 for report in reports if safetyCheck(report))
+    failed_checks = []
     for report in reports:  
-        safetyCheck(report)
+        safety_check(report, failed_checks)
+    print(failed_checks)
     #print(safeCount)
 
-def safetyCheck(report):
-    pass
+    
+
+def safety_check(report, failed_checks):
+    differences = []
+    for i in range(len(report)-1):
+        differences.append(int(report[i]) - int(report[i+1]))
+    
+    #print(differences)
+    res = (all(abs(difference) <= 3 for difference in differences)) and (all(difference > 0 for difference in differences) or all(difference < 0 for difference in differences))
+    if not(res):
+        failed_checks.append(differences)
 
 
+# if only one number is >3
+# if only one number is negative while all other pos
+# if only one number pos while all other negative 
     
 if __name__ == '__main__':
     main()
