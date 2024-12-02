0f17b1acba2d105e0595c882bfe55534aca51647
emmyan99 <emmy.andreasson@gmail.com>
2024-12-02 17:02:22 +0100

    hihi

--- day2/day2_b.py
+++ day2/day2_b.py
@@ -2,12 +2,17 @@ def main():
     reports = list(word.split() for word in open('input.txt'))
 
     safeCount = 0
-    #safeCount = sum(1 for report in reports if safetyCheck(report))
+    # safeCount = sum(1 for report in reports if safetyCheck(report))
     failed_checks = []
     for report in reports:  
         safety_check(report, failed_checks)
+    # print(failed_checks)
+    safe_count = len(reports) - len(failed_checks)
+    # print(safeCount)
     print(failed_checks)
-    #print(safeCount)
+
+    #count = make_safe(failed_checks)
+    #safe_count += count
 
     
 
@@ -19,7 +24,12 @@ def safety_check(report, failed_checks):
     #print(differences)
     res = (all(abs(difference) <= 3 for difference in differences)) and (all(difference > 0 for difference in differences) or all(difference < 0 for difference in differences))
     if not(res):
-        failed_checks.append(differences)
+        failed_checks.append((differences, report))
+
+
+def make_safe(failed_checks):
+    for check in failed_checks:
+        pass  
 
 
 # if only one number is >3
