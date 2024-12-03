3d9f52ddee526cfafa2b711dbaee8a0d26f1c82b
emmyan99 <emmy.andreasson@gmail.com>
2024-12-03 13:00:22 +0100

    hhhhhh

--- day3/day3.py
+++ day3/day3.py
@@ -1,1 +1,28 @@
 # regex mul\(\d\,\d\)
+import re
+
+def main():
+    with open('input.txt') as f:
+        input = f.read()
+    print(input)
+
+    x = re.findall("mul[\(\[]\d+,\d+[\)]", input)
+    print(x)
+    
+    toSum = []
+    for expr in x:
+        expr = expr[3:]
+        #UHHHHHHHHHHHHHHHHHHHHHH
+        print(toSum)
+
+
+    print(sum(toSum))
+
+    
+
+
+if __name__ == '__main__':
+    main()
+
+
+
