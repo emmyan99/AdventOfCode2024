1b8269d803d9dbd2f7315e20753208f7f93fd7fd
emmyan99 <emmy.andreasson@gmail.com>
2024-12-04 09:22:11 +0100

    3a done

--- day3/day3.py
+++ day3/day3.py
@@ -3,21 +3,22 @@ import re
 def main():
     with open('input.txt') as f:
         input = f.read()
-    print(input)
 
     x = re.findall("mul[\(\[]\d+,\d+[\)]", input)
     print(x)
     
     toSum = []
     for expr in x:
-        expr = expr[3:]
-        #UHHHHHHHHHHHHHHHHHHHHHH
+        expr = expr[3:] 
+        expr = expr.split(",")
+        expr[0] = expr[0][1:]
+        expr[1] = expr[1][:-1]          
+        toSum.append(int(expr[0]) * int(expr[1]))
         print(toSum)
 
-
-    print(sum(toSum))
-
-    
+    sum_ = 0
+    sum_ = sum(toSum)
+    print(sum_)
 
 
 if __name__ == '__main__':
