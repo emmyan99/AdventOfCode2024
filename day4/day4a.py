cc5f5f8d3a69067fbc9967d63c31a91eca4fc714
emmyan99 <emmy.andreasson@gmail.com>
2024-12-05 12:50:15 +0100

    moar ideas d4

--- day4/day4a.py
+++ day4/day4a.py
@@ -5,27 +5,31 @@ def main():
             input.append(list(line.strip()))
         print(input)
 
+    word = "xmas"
     #word counter
     wc = 0
 
     for row in range(len(input)):
         for col in range(len(input[0])):
             if input[row][col] == "X":
-                amount_found = check_neighbours(row, col)
+                amount_found = check_neighbours(row, col, "M")
                 wc += amount_found
 
     print(wc)
 
-
-def check_neighbours(row, col):
+#need2 catch outisde of index error
+def check_neighbours(row, col, nletter):
+    amount_found = 0 #unsure
     # ugly and hard coded for corner X for now >:(
-    if input[row][col+1] == "M":
+    if input[row][col+1] == nletter:
+        #use word[next letter to find] recursively
         print("yipii")
-    if input[row+1][col] == "M":
+    if input[row+1][col] == nletter:
         pass
-    if input[row+1][col+1] == "M":
+    if input[row+1][col+1] == nletter:
         pass
-    return 0
+    return amount_found
+# once S is found, amount_found can be incremented? 
 
 
 if __name__ == '__main__':
