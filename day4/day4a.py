899fc4f3603c5b5da1468ef1274f365994717e55
emmyan99 <emmy.andreasson@gmail.com>
2024-12-05 12:41:04 +0100

    day4 progres

--- day4/day4a.py
+++ day4/day4a.py
@@ -1,7 +1,32 @@
 def main():
+    input = []
     with open('input.txt') as f:
-        input = f.read()
-    print(input)
+        for line in f:
+            input.append(list(line.strip()))
+        print(input)
+
+    #word counter
+    wc = 0
+
+    for row in range(len(input)):
+        for col in range(len(input[0])):
+            if input[row][col] == "X":
+                amount_found = check_neighbours(row, col)
+                wc += amount_found
+
+    print(wc)
+
+
+def check_neighbours(row, col):
+    # ugly and hard coded for corner X for now >:(
+    if input[row][col+1] == "M":
+        print("yipii")
+    if input[row+1][col] == "M":
+        pass
+    if input[row+1][col+1] == "M":
+        pass
+    return 0
+
 
 if __name__ == '__main__':
     main()
