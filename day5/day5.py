eed07a497836b86bc7ebbb5fe7e6f1cf74d41966
emmyan99 <emmy.andreasson@gmail.com>
2024-12-10 20:53:26 +0100

    i am the best B)

--- day5/day5.py
+++ day5/day5.py
@@ -27,82 +27,62 @@ def main():
     mid_numbers = handle_updates(updates, rules, mid_numbers)
 
     print(sum(mid_numbers))
-    #print(rules)
-    #print('\n')
-    #print(updates)
 
-def handle_updates(updates, rules, mid_numbers): 
 
-    corrected_updates  = []   
-    
+def handle_updates(updates, rules, mid_numbers):
     for line in updates:
-        #valid_line = True # PART 1
         corrected = False
-        for number in line:
-
-            if number in rules:
-                # PART 1:
-                #if not check_order(line, number, rules):
-                    #valid_line = False
-                    #break
-                
-                while not (check_order(line, number, rules)):
-                    corrected_line = correct_order(line, number, rules)
-                    corrected = True
-                
+        
+        while not (check_order(line, rules)):   
+            line = correct_order(line, rules)
+            corrected = True
+        
         if corrected:
-            corrected_updates.append(corrected_line)
-            print("corrected line: " + str(corrected_line))
-            print("corrected updates: " + str(corrected_updates))
-    
-    print(f"Total corrected lines: {len(corrected_updates)}")
-    for line in corrected_updates:
-        mid_numbers.append(line[int(len(line)//2)])
-        #print(mid_numbers)
-
-
-        # PART 1:
-        #if valid_line:
-            #print("line" +  str(line) + " is valid ")
-            #mid_numbers.append(line[int(len(line)/2)])
-        print(mid_numbers)
-
+            mid_numbers.append(line[len(line)//2])
+            #print(mid_numbers)
 
     return mid_numbers
 
-def correct_order(line, key, rules):
-    print("handling line " + str(line))
-    for value in rules[key]:
-        if value in line:
-            value_index = line.index(value)
-            key_index = line.index(key)
-            # print(str(value) + " is at " + str(value_index) + " in line " + str(line))
-             
-            # swap places
-            if key_index > value_index:
-                print("swapping " + str(value) + " and key: " + str(key))
-                line[key_index] = value
-                line[value_index] = key
+
+def correct_order(line, rules):
+    for update_value in line:
+        if update_value in rules:
+            
+            for value in rules[update_value]:
+                if value in line:
+
+                    rule_index = line.index(update_value)
+                    value_index = line.index(value)
+
+                    if rule_index > value_index:
+                        line[rule_index] = value
+                        line[value_index] = update_value
+                        
     return line
 
-def check_order(line, key, rules):
-    for value in rules[key]:
-        if value in line:
-            value_index = line.index(value)
-            key_index = line.index(key)
 
-            # key should be before value
-            if key_index > value_index:
-                print(str(key) + " comes after " + str(value) + " in line " + str(line))
-                return False
+def check_order(line, rules):
+    #print("I will check line: "+ str(line) + " for value " + str(rule_key) + "\n" + "  Rule values are" + str(rules[rule_key]))
+    for update_value in line:
+        if update_value in rules:
             
+            for value in rules[update_value]:
+                if value in line:
+
+                    rule_index = line.index(update_value)
+                    value_index = line.index(value)
+
+                    if rule_index > value_index:
+                        return False
     return True
+
 
 def convert_rules_to_int(rules):
     int_rules = {}
     for key, value in rules.items():
         int_rules[int(key)] = [int(val) for val in value]
     return int_rules
+
 
 if __name__ == '__main__':
     main()
