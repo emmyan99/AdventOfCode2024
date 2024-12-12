1af9787e0d06e15c03f893a02fd044d032b81e8d
emmyan99 <emmy.andreasson@gmail.com>
2024-12-12 14:58:38 +0100

    hmm

--- day6/day6.py
+++ day6/day6.py
@@ -1,10 +1,17 @@
 from enum import Enum
+import numpy as np
 
 class Direction(Enum):
     UP = '^'
     DOWN = 'v'
     LEFT = '<'
     RIGHT = '>'
+
+class Step:
+    UP = np.array([-1, 0])
+    DOWN = np.array([1, 0])
+    LEFT = np.array([0, -1])
+    RIGHT = np.array([0, 1])
 
 def main ():
     map_ = []
@@ -15,8 +22,11 @@ def main ():
     row, column, direction = find_guard(map_)
     print("Guard is at " +str(row) +", "+ str(column))
 
-    steps = 1
-    amount = move_guard(row, column, map_, direction, steps)
+    steps = 0
+    while not out_of_bounds(row, column, map_):
+        steps, direction, row, column = take_step(row, column, map_, direction, steps)
+
+    #print(sum(steps))
 
 def find_guard(map_):
     for row in range(len(map_)):
@@ -31,15 +41,34 @@ def find_guard(map_):
                 return row, column, Direction.LEFT
             
             
-def move_guard(row, column, map_, direction, steps):
+def take_step(row, column, map_, direction, steps):
     OBSTACLE = "#"
 
+    current_location = np.array([row, column])
+    
     if direction == Direction.UP:
-        for i in range(row): # does ot include row 
-            if map_[i][column] == OBSTACLE:
-                print("obstacle found at " + str(i) +", "+ str(column))
-                steps += row - (i+1)
-                print(steps)
+        new_location = current_location + Step.UP
+        row, col = new_location
+        if map_[row][col] == OBSTACLE:
+            return steps, Direction.DOWN, column, row
+        if out_of_bounds:
+            return steps, Direction.UP, column, row
+        else:
+            steps += 1
+            return steps, Direction.UP, column, (row+1)
+
+    #if direction == Direction.DOWN:
+    #if direction == Direction.LEFT:
+    #if direction == Direction.RIGHT:
+
+def out_of_bounds(row, col, map_):
+    if row > len(map_) or row < 0 or col > len(map_[row]) or col < 0:
+        return True
+    return False
+
+
+
+                
 
 
 
