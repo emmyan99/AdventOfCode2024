cf4ba74e41355feb581a178f48a69ce26d2c8406
emmyan99 <emmy.andreasson@gmail.com>
2024-12-14 16:15:10 +0100

    day 6 p1 done

--- day6/day6.py
+++ day6/day6.py
@@ -7,70 +7,81 @@ class Direction(Enum):
     LEFT = '<'
     RIGHT = '>'
 
-class Step:
-    UP = np.array([-1, 0])
-    DOWN = np.array([1, 0])
-    LEFT = np.array([0, -1])
-    RIGHT = np.array([0, 1])
-
 def main ():
     map_ = []
     with open ('input.txt') as f:
         for line in f:
             map_.append(list(line.strip()))
 
-    row, column, direction = find_guard(map_)
-    print("Guard is at " +str(row) +", "+ str(column))
+    location, direction = find_guard(map_)
+    print("Guard is at " +str(location[0]) +", "+ str(location[1]))
 
-    steps = 0
-    while not out_of_bounds(row, column, map_):
-        steps, direction, row, column = take_step(row, column, map_, direction, steps)
+    steps = 1
+    locations = []
+    exited = False
+    while not exited:
+        steps, location, direction, exited = take_step(steps, location, direction, exited, map_, locations)
 
-    #print(sum(steps))
+    print(steps)
 
 def find_guard(map_):
     for row in range(len(map_)):
         for column in range(len(map_[row])):
             if map_[row][column] == Direction.UP.value:
-                return row, column, Direction.UP
+                location = np.array([row, column])
+                direction = np.array([-1, 0])
+
             elif map_[row][column] == Direction.DOWN.value:
-                return row, column, Direction.DOWN
+                location = np.array([row, column])
+                direction = np.array([1, 0])
+
             elif map_[row][column] == Direction.RIGHT.value:
-                return row, column, Direction.RIGHT
-            elif map_[row][column] == Direction.LEFT.value:
-                return row, column, Direction.LEFT
+                location = np.array([row, column])
+                direction = np.array([0, 1])
+
+            elif map_[row][column] == Direction.LEFT.value:                        
+                location = np.array([row, column])
+                direction = np.array([0, -1])
+
+    return location, direction
             
             
-def take_step(row, column, map_, direction, steps):
+def take_step(steps, location, direction, exited, map_, locations):
     OBSTACLE = "#"
 
-    current_location = np.array([row, column])
-    
-    if direction == Direction.UP:
-        new_location = current_location + Step.UP
-        row, col = new_location
-        if map_[row][col] == OBSTACLE:
-            return steps, Direction.DOWN, column, row
-        if out_of_bounds:
-            return steps, Direction.UP, column, row
-        else:
-            steps += 1
-            return steps, Direction.UP, column, (row+1)
+    locations.append(location)
 
-    #if direction == Direction.DOWN:
-    #if direction == Direction.LEFT:
-    #if direction == Direction.RIGHT:
+    switchDir = {
+        (-1,0) : (0,1), # up -> right
+        (0,1) : (1,0), # right -> down
+        (1,0) : (0,-1), # down -> left
+        (0,-1) : (-1,0), # left -> up
+    }
 
-def out_of_bounds(row, col, map_):
-    if row > len(map_) or row < 0 or col > len(map_[row]) or col < 0:
+    new_location = location + direction
+
+    if out_of_bounds(new_location, map_):
+        exited = True
+        return steps, location, direction, exited
+    elif map_[new_location[0]][new_location[1]] == OBSTACLE:
+        #print("Obstacle found at " + str(new_location[0]) + " " + str(new_location[1]))
+        dir_tuple = (direction[0], direction[1])
+        new_dir = switchDir[dir_tuple]
+        direction = np.array([new_dir[0], new_dir[1]])
+        #print(direction)
+    else:
+        #print("inc step at " + str(new_location[0]) + " " + str(new_location[1]))
+        visited = any(np.array_equal(arr, new_location) for arr in locations)
+        if not visited: steps += 1
+        location = new_location
+
+    return steps, location, direction, exited
+
+
+def out_of_bounds(location, map_):
+    if location[0] > (len(map_)-1) or location[0] < 0 or location[1] > (len(map_)-1) or location[1] < 0:
         return True
     return False
-
-
-
-                
-
-
 
 if __name__ == '__main__':
     main()
