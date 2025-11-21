6f802990a1ce97d7351a9b9b68b74fcda4cfc375
emmyan99 <emmy.andreasson@gmail.com>
2025-11-21 11:42:27 +0100

    ummm

--- day6/day6b.py
+++ day6/day6b.py
@@ -7,3 +7,86 @@ class Direction(Enum):
     LEFT = '<'
     RIGHT = '>'
 
+def main ():
+    map_ = []
+    with open ('input.txt') as f:
+        for line in f:
+            map_.append(list(line.strip()))
+
+    location, direction = find_guard(map_)
+    print("Guard is at " +str(location[0]) +", "+ str(location[1]))
+
+    steps = 1
+    locations = []
+    exited = False
+    while not exited:
+        steps, location, direction, exited = take_step(steps, location, direction, exited, map_, locations)
+        
+
+    # horizontal and vertical counter?
+    # if about to build a square -> found a spot for an obstacle
+    # 
+
+    print(steps)
+
+def find_guard(map_):
+    for row in range(len(map_)):
+        for column in range(len(map_[row])):
+            if map_[row][column] == Direction.UP.value:
+                location = np.array([row, column])
+                direction = np.array([-1, 0])
+
+            elif map_[row][column] == Direction.DOWN.value:
+                location = np.array([row, column])
+                direction = np.array([1, 0])
+
+            elif map_[row][column] == Direction.RIGHT.value:
+                location = np.array([row, column])
+                direction = np.array([0, 1])
+
+            elif map_[row][column] == Direction.LEFT.value:                        
+                location = np.array([row, column])
+                direction = np.array([0, -1])
+
+    return location, direction
+            
+            
+def take_step(steps, location, direction, exited, map_, locations):
+    OBSTACLE = "#"
+
+    locations.append(location)
+
+    switchDir = {
+        (-1,0) : (0,1), # up -> right
+        (0,1) : (1,0), # right -> down
+        (1,0) : (0,-1), # down -> left
+        (0,-1) : (-1,0), # left -> up
+    }
+
+    new_location = location + direction
+
+    if out_of_bounds(new_location, map_):
+        exited = True
+        return steps, location, direction, exited
+    elif map_[new_location[0]][new_location[1]] == OBSTACLE:
+        #print("Obstacle found at " + str(new_location[0]) + " " + str(new_location[1]))
+        dir_tuple = (direction[0], direction[1])
+        new_dir = switchDir[dir_tuple]
+        direction = np.array([new_dir[0], new_dir[1]])
+        #print(direction)
+    else:
+        #print("inc step at " + str(new_location[0]) + " " + str(new_location[1]))
+        visited = any(np.array_equal(arr, new_location) for arr in locations)
+        if not visited: steps += 1
+        location = new_location
+
+    return steps, location, direction, exited
+
+
+def out_of_bounds(location, map_):
+    if location[0] > (len(map_)-1) or location[0] < 0 or location[1] > (len(map_)-1) or location[1] < 0:
+        return True
+    return False
+
+if __name__ == '__main__':
+    main()
